7414b6b4780f4103179f161916390f8ef925b9c1
Tim Pietz <tim@pietz.me>
2022-02-27 17:27:02 +0100

    Create all required directories on startup

--- tld/config.py
+++ tld/config.py
@@ -7,3 +7,6 @@ train_output_dir = data_dir / 'outputs'
 train_results_dir = data_dir / 'results'
 link_embedding_dir = data_dir / 'link-embeddings'
 word_embedding_dir = data_dir / 'word-embeddings'
+
+for directory in [preprocessed_data_dir, train_output_dir, train_results_dir, link_embedding_dir, word_embedding_dir]:
+    directory.mkdir(parents=True, exist_ok=True)
