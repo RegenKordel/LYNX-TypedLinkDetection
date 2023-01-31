d3aad1b14c742f979b8caf08fef8d4b56731e5f9
ClaraMarieLueders <clara.marie.lueders@gmail.com>
2023-01-31 15:36:49 +0100

    Add files via upload

--- tld/linktypes.py
+++ tld/linktypes.py
@@ -1,6 +1,8 @@
 from typing import Literal, Dict
 
 fine_linktype_map = {
+    'Account': 'Impacts',
+    
     'Backports': 'Backport', 
 
     'Blocked': 'Block',
@@ -24,8 +26,10 @@ fine_linktype_map = {
     'Parent-Relation': 'Parent-Child',
 
     'Cloners': 'Clone', 
-    'Cloners (old)': 'Clone',
-    'Cloners (migrated)': 'Clone',
+    'Cloners (old)': 'Clone', 
+    'Cloners (migrated)' : 'Clone',
+    
+    'Covered': 'Cover',
 
     'Collection': 'Incorporate', 
     'Container': 'Incorporate',
@@ -58,6 +62,7 @@ fine_linktype_map = {
 
     'Epic': 'Epic', 
     'Epic-Relation': 'Epic',
+    'Initiative': 'Epic',
     
     'Finish-to-Finish link (WBSGantt)': 'finish-finish', 
     'Gantt End to End': 'finish-finish', 
@@ -69,14 +74,16 @@ fine_linktype_map = {
     'finish-start [GANTT]': 'finish-start',
 
     'Gantt Start to Start': 'start-start', 
-    
+    'Gantt: start-start': 'start-start',
+   
     'Gantt: start-finish': 'start-finish', 
+    'start-finish [GANTT]': 'start-finish', 
     
     'Follows': 'Follow', 
     'Sequence': 'Follow', 
     
     'Implement': 'Implement', 
-    'Implements': 'Implements', 
+    'Implements': 'Implement', 
     
     'Issue split': 'Split',
     'Split': 'Split',
@@ -106,8 +113,6 @@ fine_linktype_map = {
     'Tested': 'Test',
     
     'Trigger': 'Trigger', 
-    
-    'Non-Link': 'Non-Link',
           
     '1 - Relate': 'Relate',
     '5 - Depend':   'Depend',          
@@ -119,31 +124,74 @@ fine_linktype_map = {
     'Verify': 'Verify'
 }
 
+# fine_linktype_to_category_map = {
+#     'Backport': 'Workflow',
+#     'Block': 'Causal',
+#     'Bonfire Testing': 'Workflow',
+#     'Breaks': 'Causal',
+#     'Cause': 'Causal',
+#     'Clone': 'Duplicate',
+#     'Cover': 'Workflow',
+#     'Depend': 'Causal',
+#     'Derive': 'Workflow',
+#     'Detail': 'Workflow',
+#     'Documented': 'Workflow',
+#     'Duplicate': 'Duplicate',
+#     'Epic': 'Composition',
+#     'Fix': 'Workflow',
+#     'Follow': 'Causal',
+#     'Impacts': 'Causal',
+#     'Incorporate': 'Composition',
+#     'Parent-Child': 'Composition',
+#     'Precede': 'Causal',
+#     'Relate': 'General',
+#     'Replace': 'Workflow',
+#     'Require': 'Causal',
+#     'Split': 'Composition',
+#     'Subtask': 'Composition',
+#     'Supercede': 'Causal',
+#     'Test': 'Workflow',
+#     'Trigger': 'Workflow',
+#     'finish-start': 'Causal',
+#     'finish-finish': 'Causal',
+#     'start-start': 'Causal',
+#     'start-finish': 'Causal',
+#     'Verify': 'Workflow'
+# }
+
 fine_linktype_to_category_map = {
-    'Block': 'Causal',
-    'Bonfire Testing': 'Workflow',
-    'Breaks': 'Causal',
-    'Cause': 'Causal',
-    'Clone': 'General',
-    'Depend': 'Causal',
-    'Detail': 'Workflow',
-    'Documented': 'Workflow',
-    'Duplicate': 'General',
-    'Epic': 'Epic',
-    'Fix': 'Workflow',
-    'Follow': 'Causal',
-    'Incorporate': 'Split',
-    'Parent-Child': 'Split',
-    'Relate': 'General',
-    'Replace': 'General',
-    'Require': 'Causal',
-    'Split': 'Split',
-    'Subtask': 'Split',
-    'Supercede': 'Causal',
-    'Trigger': 'Workflow',
-    'finish-start': 'Causal',
-    'Non-Link': 'Non-Link',
-    'Verify': 'Workflow'
+    'Backport': 'Linked',
+    'Block': 'Linked',
+    'Bonfire Testing': 'Linked',
+    'Breaks': 'Linked',
+    'Cause': 'Linked',
+    'Clone': 'Linked',
+    'Cover': 'Linked',
+    'Depend': 'Linked',
+    'Derive': 'Linked',
+    'Detail': 'Linked',
+    'Documented': 'Linked',
+    'Duplicate': 'Linked',
+    'Epic': 'Linked',
+    'Fix': 'Linked',
+    'Follow': 'Linked',
+    'Impacts': 'Linked',
+    'Incorporate': 'Linked',
+    'Parent-Child': 'Linked',
+    'Precede': 'Linked',
+    'Relate': 'Linked',
+    'Replace': 'Linked',
+    'Require': 'Linked',
+    'Split': 'Linked',
+    'Subtask': 'Linked',
+    'Supercede': 'Linked',
+    'Test': 'Linked',
+    'Trigger': 'Linked',
+    'finish-start': 'Linked',
+    'finish-finish': 'Linked',
+    'start-start': 'Linked',
+    'start-finish': 'Linked',
+    'Verify': 'Linked'
 }
 
 category_map = {
@@ -153,7 +201,6 @@ category_map = {
 }
 
 non_link_name = 'Non-Link'
-
 
 Target = Literal['linktype', 'category']
 
